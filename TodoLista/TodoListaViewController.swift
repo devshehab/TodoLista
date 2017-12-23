77e04dedb1a8faea83b7623005026d35c05a32b1
shehab <devshehab@gmail.com>
2017-12-23 09:35:15 +0300

    TODOLISTA complete with dataSource and delegate methods { TableViews, check marks, pressing highlight, Navigation bar}

---
+++ TodoLista/TodoListaViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  TodoLista
+//
+//  Created by Mac on 12/22/17.
+//  Copyright © 2017 shehaboli. All rights reserved.
+//
+
+import UIKit
+
+class TodoListaViewController: UITableViewController {
+
+    
+    let itemArray = ["Find Meimwh", "kiss her", "hit her"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+            return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+  
+
+}
+
