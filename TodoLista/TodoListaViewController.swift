027c37c85f49148514259d0248fc67b361a578d5
Shehab Ahmed <shehab2014kuwait@gmail.com>
2017-12-24 16:52:12 +0300

    persisting Data using Codable and Plist

--- TodoLista/TodoListaViewController.swift
+++ TodoLista/TodoListaViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class TodoListaViewController: UITableViewController {
 
-    //VARAIBLES AND LETS@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //VARAIBLES AND LETS   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     var itemArray = [Item]()
+     let dataFilePath = FileManager.default.urls(for:.documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
+ 
     
-    let defaults = UserDefaults.standard
-    
-    //TABLE VIEW METHODS@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //TABLE VIEW METHODS   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -34,19 +34,19 @@ class TodoListaViewController: UITableViewController {
         return cell
     }
     
-    
+    //FOR HIGHLIGHTING AND CHECKMARKS  @@@@@@@@@@@@@@@@@@@@@@@@@
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
       
+        save()
         
-        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     
-    //IBACTIONS@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //IBACTIONS  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add New ToDoLista Item", message: "", preferredStyle: .alert)
@@ -60,9 +60,8 @@ class TodoListaViewController: UITableViewController {
             
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "TodoListaArray")
+            self.save()
             
-            self.tableView.reloadData()
         }
         
         alert.addTextField { (alertTextField) in
@@ -79,29 +78,44 @@ class TodoListaViewController: UITableViewController {
     
     
     
-    //VIEW DID LOAD!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //VIEW DID LOAD!  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Find Meimwh"
-        itemArray.append(newItem)
         
-        let newItem2 = Item()
-        newItem2.title = "hi meimw"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "hit Meimwh"
-        itemArray.append(newItem3)
+        loadItems()
         
         
-        if let items = defaults.array(forKey: "TodoListaArray") as? [Item] {
-           itemArray = items
+    }
+//FUNCTION  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    
+    func save() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }
+        catch {
+            print("Error Encoding Item Array, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+  
+    func loadItems() {
+
+        if let data = try? Data(contentsOf: dataFilePath!) {
+        
+            let decoder = PropertyListDecoder()
+            do {
+           itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch {
+                print(error)
+            }
         }
     }
-
-  
 
 }
 
